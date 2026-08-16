afaf89d9576f974e2e7d1f971bedf239ff97f1f0
Niharika Muzumdar <niharikamuzumdar1992@gmail.com>
2026-08-16 23:34:40 +0530

    basic java programs

---
+++ basics/DataTypes.java
@@ -0,0 +1,26 @@
+class DataTypes{
+    public static void main(String[] args){
+
+        byte age = 25;
+        short year = 2004;
+
+        int salary = 35000;
+        long population = 23400000000L;
+
+        float percentage = 3.2f;
+        double price = 45500.77;
+
+        char grade = 'A';
+        boolean isActive = true;
+
+        System.out.println("age : " + age);
+        System.out.println("year : " + year);
+        System.out.println("salary : " +salary);
+        System.out.println("population : " +population);
+        System.out.println("percentage : " +percentage);
+        System.out.println("price : " + price);
+        System.out.println("grade : " +grade);
+        System.out.println("isActive : " + isActive);
+
+    }
+}
